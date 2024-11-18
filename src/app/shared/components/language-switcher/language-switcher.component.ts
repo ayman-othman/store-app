e081ed1defc2591eb6c82848a048c57c0290285e
Ayman Mostafa <71860474+ayman-othman@users.noreply.github.com>
2024-11-18 09:55:44 +0200

    language switcher

---
+++ src/app/shared/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { LanguageService } from '../../../core/services/language/language.service';
+import { LANGUAGE } from '../../../core/models/language/language.const';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { ProjectLanguage } from '../../../core/models/language/language.type';
+import { ICONS } from '../../../core/models/icons/icon.const';
+
+@Component({
+  selector: 'language-switcher',
+  standalone: true,
+  imports: [MatMenuModule, CommonModule, TranslateModule ,SvgIconComponent],
+
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.scss',
+})
+export class LanguageSwitcherComponent {
+  // Inject
+  private _languageService = inject(LanguageService);
+  public translateService = inject(TranslateService);
+  // Public
+  public LANGUAGE = LANGUAGE;
+  public ICONS = ICONS;
+
+  
+  public setCurrentLanguage(selectedLanguage: ProjectLanguage): void {
+    location.reload();
+    this._languageService.setCurrentLanguage(selectedLanguage);
+  }
+}
