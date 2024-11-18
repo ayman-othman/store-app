a323b56d3be6f2047ffb750ddf107c545b19e2dd
Ayman Mostafa <71860474+ayman-othman@users.noreply.github.com>
2024-11-18 11:12:31 +0200

    chip

---
+++ src/app/shared/components/chip/chip.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { Component, input, InputSignal } from "@angular/core";
+import { FadeColorPipe } from "@e-commerce-store/shared/pipes/fade-color/fade-color.pipe";
+
+@Component({
+	selector: "chip",
+	standalone: true,
+	imports: [CommonModule, FadeColorPipe],
+	templateUrl: "./chip.component.html",
+	styleUrl: "./chip.component.scss",
+})
+export class ChipComponent {
+	public colorHex: InputSignal<string> = input.required();
+}
