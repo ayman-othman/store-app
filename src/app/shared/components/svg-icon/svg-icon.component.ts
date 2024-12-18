5538e6b7ff1cd80e8ff1fa9bd1d9f70509672bea
Ayman Mostafa <71860474+ayman-othman@users.noreply.github.com>
2024-12-18 11:39:12 +0200

    enhance icons component

--- src/app/shared/components/svg-icon/svg-icon.component.ts
+++ src/app/shared/components/svg-icon/svg-icon.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, InputSignal } from '@angular/core';
+import {
+  Component,
+  Input,
+  input,
+  InputSignal,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { ICONS, Icons } from '../../../core/models/icons/icon.const';
 
 @Component({
@@ -7,13 +14,28 @@ import { ICONS, Icons } from '../../../core/models/icons/icon.const';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './svg-icon.component.html',
-  styleUrl: './svg-icon.component.scss'
+  styleUrl: './svg-icon.component.scss',
 })
-export class SvgIconComponent {
+export class SvgIconComponent implements OnChanges {
   // Inputs
   public icon: InputSignal<Icons> = input.required<Icons>();
   public size: InputSignal<number> = input<number>(3);
+  @Input() fill: string | undefined;
 
   // Public
-  public ICONS = ICONS
+  public ICONS = ICONS;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['fill']?.currentValue) {
+      this.fillStyles;
+    }
+  }
+
+  get fillStyles() {
+    const styles: { [key: string]: string } = {};
+    for (let i = 1; i <= 15; i++) {
+      styles[`--color${i}`] = this.fill!;
+    }
+    return styles;
+  }
 }
